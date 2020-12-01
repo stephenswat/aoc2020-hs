e0d2e3f074ea5cc39b3ebc15f0ddf17bc1e8ee96
Stephen Nicholas Swatman <stephen@v25.nl>
2020-12-01 13:07:30 +0100

    Refactor solution for day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,14 +1,25 @@
 import System.Environment
 
+type Solution = String -> String
+
+parseNumbers :: String -> [Integer]
+parseNumbers = map read . lines
+
+problemA :: [Integer] -> Integer
+problemA i = head [a * b | a <- i, b <- i, a + b == 2020]
+
+problemB :: [Integer] -> Integer
+problemB i = head [a * b * c | a <- i, b <- i, c <- i, a + b + c == 2020]
+
+day01 :: (Solution, Solution)
+day01 = (show . problemA . parseNumbers, show . problemB . parseNumbers)
+
 main :: IO ()
 main = do {
     args <- getArgs;
     file <- readFile (head args);
-    let nums = map (read :: String -> Integer) . lines $ file
-        ans1 = [a * b | a <- nums, b <- nums, a + b == 2020]
-        ans2 = [a * b * c | a <- nums, b <- nums, c <- nums, a + b + c == 2020]
-    in do {
-        putStrLn . ("Problem 1: " ++) . show . head $ ans1;
-        putStrLn . ("Problem 2: " ++) . show . head $ ans2;
+    let (solA, solB) = day01 in do {
+        putStrLn . ("Problem 1: " ++) . solA $ file;
+        putStrLn . ("Problem 2: " ++) . solB $ file;
     }
 }
