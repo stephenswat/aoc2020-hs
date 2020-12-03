398e3cfb38c733d8f34d299b73f054f2c73012fb
Stephen Nicholas Swatman <stephen@v25.nl>
2020-12-03 12:12:29 +0100

    Refactor solution to day 3

--- src/Problems/Day03.hs
+++ src/Problems/Day03.hs
@@ -1,37 +1,36 @@
 module Problems.Day03 (solution) where
 
-import Data.Set
+import Data.Set (Set, fromList, member)
+import Data.Bifunctor
 
 import Common
 
-data Map = Map {
+data World = World {
     trees :: Set (Int, Int),
     height :: Int,
     width :: Int
 }
 
-parseMap :: String -> Map
-parseMap i = Map { trees=fromList trees, height=h, width=w }
+parseWorld :: String -> World
+parseWorld i = World { trees=fromList trees, height=h, width=w }
     where
         rows = lines i
         h = length rows
         w = length . head $ rows
-        trees = [(x, y) | x <- [0..w - 1], y <- [0..h - 1], (rows !! y) !! x == '#']
+        trees = [(x, y) | x <- [0..w-1], y <- [0..h-1], (rows !! y) !! x == '#']
 
-countSlope :: (Int, Int) -> (Int, Int) -> Map -> Int
-countSlope c@(x, y) d@(dx, dy) m
-    | y >= height m = 0
-    | isTree c m    = 1 + next
-    | otherwise     = next
-    where
-        isTree (x, y) m = member (x `mod` width m, y) $ trees m
-        next = countSlope (x + dx, y + dy) d m
-
-slopes :: [(Int, Int)]
-slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+countTrees :: (Int, Int) -> World -> Int
+countTrees (dx, dy) m
+    = length
+    . filter (isTree m)
+    . takeWhile ((<= height m) . snd)
+    . iterate (bimap (+ dx) (+ dy))
+    $ (0, 0)
+    where isTree m (x, y) = member (x `mod` width m, y) $ trees m
 
 solution :: Day
 solution =
-    ( show . countSlope (0, 0) (3, 1) . parseMap
-    , show . product . (\m -> [countSlope (0, 0) s m | s <- slopes]) . parseMap
+    ( show . countTrees (3, 1) . parseWorld
+    , show . product . (\m -> [countTrees s m | s <- slopes]) . parseWorld
     )
+    where slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
