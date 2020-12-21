2f25a8ebfbee855af64b7ba8c2a6244fc9aa307d
Stephen Nicholas Swatman <stephen@v25.nl>
2020-12-22 00:00:19 +0100

    Improve solver

--- src/Common/Solver.hs
+++ src/Common/Solver.hs
@@ -1,22 +1,18 @@
 module Common.Solver where
 
-import Data.Set (Set, size, empty, filter, toList, unions)
-import Data.Map (Map, fromList, elems, toList, map)
-import Data.Maybe (isJust)
+import Data.Set (Set, size, filter, toList)
+import Data.Map (Map, elems)
+import Data.Either (isRight, rights)
 
 optionSolver :: (Ord a, Ord b) => Map a (Set b) -> Maybe (Map a b)
-optionSolver = go . Data.Map.map (\x -> (Nothing, x))
+optionSolver i
+    | and . fmap isRight . elems $ p = Just . fmap (\(Right a) -> a) $ p
+    | otherwise = Nothing
     where
-        go :: (Ord a, Ord b) => Map a (Maybe b, Set b) -> Maybe (Map a b)
-        go i
-            | done      = Just (Data.Map.fromList [(j, m) | (j, (Just m, _)) <- Data.Map.toList i])
-            | i == n    = Nothing
-            | otherwise = go n
-            where
-                reduce f (Nothing, xs)
-                    | size xs == 1 = (Just (head . Data.Set.toList $ xs), empty)
-                    | otherwise = (Nothing, Data.Set.filter ((flip notElem) f) xs)
-                reduce _ j                 = j
-                done = and [isJust . fst $ j | j <- elems i]
-                finished = unions . Prelude.filter ((== 1) . size) . Prelude.map snd . elems $ i
-                n = Data.Map.map (reduce finished) $ i
+        p = go . fmap Left $ i
+        go j = if j == n then j else go n where
+            n = fmap iter $ j
+            iter (Left x)
+                | size x == 1 = Right . head . toList $ x
+                | otherwise = Left . Data.Set.filter ((flip notElem) . rights . elems $ j) $ x
+            iter q = q
