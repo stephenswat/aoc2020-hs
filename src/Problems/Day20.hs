828b17184bd8b79fe555e451319cbd0928890833
Stephen Nicholas Swatman <stephen@v25.nl>
2020-12-22 14:26:51 +0100

    Clean up day 20

--- src/Problems/Day20.hs
+++ src/Problems/Day20.hs
@@ -1,10 +1,8 @@
 module Problems.Day20 (solution) where
 
-import Control.Arrow ((&&&))
-import Data.Bifunctor (bimap, second)
 import Data.List (nub)
 import Data.List.Split (splitOn)
-import Data.Map (Map, fromList, lookup, map, empty, insertWith, keys, elems, filter)
+import Data.Map (Map, fromList, lookup, empty, insertWith, keys, elems, filter)
 import Data.Maybe (fromMaybe)
 
 import Common.World
@@ -25,13 +23,6 @@ instance Tile Pixel where
 
     showTile On = '#'
     showTile Off = '.'
-
-data SolveState = SolveState {
-    sol :: Map (Integer, Integer) Integer
-}
-
-count :: Ord a => [a] -> Map a Integer
-count i = foldl (\a n -> insertWith (+) n 1 a) empty i
 
 edges :: World Pixel -> [[Pixel]]
 edges m = nub $ q ++ (Prelude.map reverse q)
@@ -54,11 +45,10 @@ parseTiles
     . splitOn "\n\n"
 
 butt :: Map Integer (World Pixel) -> Map Integer (World Pixel)
-butt m
-    = Data.Map.filter ((== 4) . length . Prelude.filter (\y -> elem y l) . edges)
-    $ m
+butt m = Data.Map.filter ((== 4) . length . Prelude.filter (\y -> elem y l) . edges) $ m
     where
-        l = keys . Data.Map.filter (== 1) . count . concat . Prelude.map edges . elems $ m
+        q a n = insertWith (+) a (1 :: Integer) n
+        l = keys . Data.Map.filter (== 1) . foldr q empty . concat . Prelude.map edges . elems $ m
 
 findSeed :: Map Integer (World Pixel) -> Integer
 findSeed = head . keys . butt
